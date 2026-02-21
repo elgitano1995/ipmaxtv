0142f41df6af86b2f1374a7fa0c289072633de71
elgitano1995 <b.mouhaid@gmail.com>
2026-02-21 12:44:57 +0000

    chore: Set default theme to light mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,8 +32,7 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
-          enableSystem
+          defaultTheme="light"
           disableTransitionOnChange
         >
           <Header />
